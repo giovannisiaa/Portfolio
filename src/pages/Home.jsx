d186c53a649bc6a8241df6aa423878eb10e6b6dd
giovannisiaa <siagiovanni1526@gmail.com>
2025-01-04 13:06:23 -0300

    refactor components to use new directory structure and implement About and Header components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Header from "../component/Header/Header";
-import About from "../component/About/About";
-import Projects from "../component/Projects/Projects";
-import Technologies from "../component/Technologies/Technologies";
-import Contact from "../component/Contact/Contact";
+import Header from "../components/Header/Header";
+import About from "../components/About/About";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
+import Contact from "../components/Contact/Contact";
 import { useDarkMode } from "../contexts/DarkModeContext";
 
 const Home = () => {
