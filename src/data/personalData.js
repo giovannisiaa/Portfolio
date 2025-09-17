0ab8cde811e8c38be18aadf03495d8a344a99b14
Soy Giovanni <76817363+giovannisiaa@users.noreply.github.com>
2025-09-17 20:48:16 -0300

    update resumeCV link in personalData to point to the correct Google Drive file

--- src/data/personalData.js
+++ src/data/personalData.js
@@ -5,7 +5,7 @@ const  personalData = {
     "github": "https://github.com/giovannisiaa",
     "linkedin": "https://www.linkedin.com/in/giovanni-sia/"
   },
-  "resumeCV": "https://drive.google.com/uc?export=download&id=1hvcMrpAarRpQmTnah0bFCk25d-cO8VRw"
+  "resumeCV": "https://drive.google.com/file/d/1HhsaHTeHj0r6l27ZUI5RK0MJc_K8MfYh/view?usp=sharing"
 }
 
 export default personalData;
